59af97877b4d629e85f0fd80092a277719059192
Dipjyoti Chakraborty <dipjyotichakraborty6@gmail.com>
2018-05-29 23:03:17 +0530

    Fourth time

---
+++ App_Code/artist_reg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+public class artist_reg
+{
+    public static bool savedata(string qry)
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True");
+        con.Open();
+        SqlCommand cmd = new SqlCommand(qry, con);
+        cmd.ExecuteNonQuery();
+        con.Close();
+        return true;
+    }
+    public static DataSet fetchdata(string qry)
+     {
+         SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True");
+         con.Open();
+         SqlDataAdapter da = new SqlDataAdapter(qry, con);
+         DataSet ds = new DataSet();
+         da.Fill(ds);
+         con.Close();
+         return ds;
+     }
+}
